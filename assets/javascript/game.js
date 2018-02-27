f04d3c3ed1676878871e0a4713387459a179e59f
olivas1406 <olivas1406@msn.com>
2018-02-27 00:41:12 -0700

    needs formatting, mostly JS updates/functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,38 @@
 var rick = {                                                                        // Object with Rick's stats
-HP: 50,
+HP: 500,
 Attack: 120,
 CounterAttack: 130,
 };
 
 var morty = {                                                                       // Object with Morty's stats
-HP: 75,
+HP: 705,
 Attack: 90,
 CounterAttack: 100
 };
 
 var krom = {                                                                        // Object with Krombopulos' stats
-HP: 90,
+HP: 900,
 Attack: 100,
 CounterAttack: 110
 };
 
 var lucius = {                                                                      // Object with Lucius' stats
-HP: 120,
+HP: 1200,
 Attack: 60,
 CounterAttack: 80
 };
 
 var move1 = 0;
 
+var move2 = true;
+
+var yourChar = {};
+
+var yourDefend = {};
+
+
 $(document).ready(function(){
-   
+
     $("#HP1").html("HP: " + (rick.HP));                                                 // Push Rick's HP to the HTML
 
     $("#HP2").html("HP: " + (morty.HP));                                                // Push Morty's HP to the HTML
@@ -36,70 +43,95 @@ $(document).ready(function(){
 
     });
 
+    
+    
+    function words() {
+        $(".pos2").append('<h4>Your Character</h4>');  // create 'your character' in the HTML                   
+        $(".pos3").append("<h4>Choose Your Opponent</h4>")  // create 'your opponent' in HTML                  
+        $(".pos3").append("<h4>---------- Defender ------------------------</h4>")      // add 'defender' to HTML4
+        $(".attack").append('<img src="./assets/images/button.png">');   //create 'attack' button in HTML
+    }
+
     function rick1() {
         if (move1 === 0) {                      // if first time rick is clicked
-        $(".r1c1").animate({top: "+=185px"}, .1);         // move rick to pos1
-        $(".r1c2, .r1c3, .r1c4").animate({top: "+=400px"}, .1); // move everyone else to pos2
-        $(".r1c2").animate({right: "+=135px"}, .1);
-        $(".r1c3").animate({right: "+=138px"}, .1);
-        $(".r1c4").animate({right: "+=140px"}, .1);
-        $(".chooseYou").remove();
-        move1 = 1234;                           // set move 1 to alert if rick is clicked again
-            
+        $(".r1c1").animate({top: "+=125px"}, .1);         // move rick to pos1
+        $(".r1c2, .r1c3, .r1c4").animate({top: "+=315px"}, .1); // move everyone else to pos2
+        $(".r1c2").animate({right: "+=135px"}, .1);             
+        $(".r1c3").animate({right: "+=138px"}, .1);            
+        $(".r1c4").animate({right: "+=140px"}, .1);             
+        $(".chooseYou").remove();                               // remove 'choose your character' from HTML
+        move1 = 1234;                           // set var move1 to alert if rick is clicked again
+        yourChar = rick;                                // var yourChar to rick stats
+        words();
+        
         } else if (move1 === 1234) {            // else if second time rick is clicked
-            alert("I'm Pickle Rick!!!");
-            move1 = 1111;
-        } else if ((move1 === 2134) || (move1 === 3124) || (move1 === 4123)) { // else if someone else is in pos1, move rick to pos3
-            $(".r1c1").animate({top: "+=200px"}, .1);   
-        } 
+            alert("I'm Pickle Rick!!!");        // you click rick again after selecting him for your character
+        } else if ((move1 === 2134) || (move1 === 3124) || (move1 === 4123)) { // else if someone else is in pos1, move rick pos2 to pos3 (defender)
+            $(".r1c1").animate({top: "+=240px"}, .1);   // move rick to position 3
+            yourDefend = rick;              // set var yourDefend to rick stats
+            
+        }  
     };
 
-    function morty1() {
-        if (move1 === 0) { 
-        $(".r1c2").animate({top: "+=185px"}, .1);          // move morty to pos1
-        $(".r1c1, .r1c3, .r1c4").animate({top: "+=400px"}, .1);
-        $(".r1c2").animate({right: "+=135px"}, .1);
+    function morty1() {                     
+        if (move1 === 0) {                          // if first time morty clicked
+        $(".r1c2").animate({top: "+=125px"}, .1);          // move morty to pos1
+        $(".r1c1, .r1c3, .r1c4").animate({top: "+=315px"}, .1); // move everyone else to pos2
+        $(".r1c2").animate({right: "+=135px"}, .1);     
         $(".r1c3").animate({right: "+=138px"}, .1);
         $(".r1c4").animate({right: "+=140px"}, .1);
-        $(".chooseYou").remove();
-        move1 = 2134;
-        } else if (move1 === 2134) {
+        $(".chooseYou").remove();           // remove 'choose your character' from HTML
+        move1 = 2134;               // set var move1 to alert if morty is clicked again
+        yourChar = morty;                   // var yourChar to morty stats
+        words();
+        } else if (move1 === 2134) {                    // else if second time morty clicked
             alert("Nobody exists on purpose, nobody belongs anywhere, we are all going to die...");
-        } else if ((move1 === 1234) || (move1 === 3124) || (move1 === 4123)){
-            $(".r1c2").animate({top: "+=200px"}, .1);
-            
+        } else if ((move1 === 1234) || (move1 === 3124) || (move1 === 4123)){  //else if someone else is in pos1, move morty pos2 to pos3(defender)
+            $(".r1c2").animate({top: "+=240px"}, .1);   // move morty to position 3
+            yourDefend = morty;                         // set var yourDefend to morty stats
         }
     };
 
     function krom1() {
         if (move1 === 0) {
-        $(".r1c3").animate({top: "+=185px"}, .1);           // move krom to pos1
-        $(".r1c1, .r1c2, .r1c4").animate({top: "+=400px"}, .1);
+        $(".r1c3").animate({top: "+=125px"}, .1);           // move krom to pos1
+        $(".r1c1, .r1c2, .r1c4").animate({top: "+=315px"}, .1);
         $(".r1c2").animate({right: "+=2px"}, .1);
         $(".r1c3").animate({right: "+=270px"}, .1);
         $(".r1c4").animate({right: "+=140px"}, .1);
         $(".chooseYou").remove();
         move1 = 3124;
+        yourChar = krom
+        words();
         } else if (move1 === 3124) {
             alert("Here I go killing again");
-        } else if ((move1 === 1234) || (move1 === 2134) || (move1 === 4123))
-            $(".r1c3").animate({top: "+=200px"}, .1);
+        } else if ((move1 === 1234) || (move1 === 2134) || (move1 === 4123)) {
+            $(".r1c3").animate({top: "+=240px"}, .1);
+            yourDefend = krom;
+        }
     };
 
     function luc1() {
         if (move1 === 0) {
-        $(".r1c4").animate({top: "+=185px"}, .1);           // move luc to pos1
-        $(".r1c1, .r1c2, .r1c3").animate({top: "+=400px"}, .1);
+        $(".r1c4").animate({top: "+=125px"}, .1);           // move luc to pos1
+        $(".r1c1, .r1c2, .r1c3").animate({top: "+=315px"}, .1);
         $(".r1c4").animate({right: "+=405px"}, .1);
         $(".chooseYou").remove();
         move1 = 4123;
+        yourChar = lucius;
+        words();
         } else if (move1 === 4123) {
             alert("I just want to go back to hell, where everyone thinks I'm smart and funny");
-        } else if ((move1 === 1234) || (move1 === 2134) || (move1 === 3124))
-        $(".r1c4").animate({top: "+=200px"}, .1);
+        } else if ((move1 === 1234) || (move1 === 2134) || (move1 === 3124)) {
+        $(".r1c4").animate({top: "+=240px"}, .1);
+        yourDefend = lucius;
+        }
     };
 
-
+    $(".resetB").append('<img src="./assets/images/reset.png" class="reset">');   //create 'attack' button in HTML     
+    $(".reset").on("click", function() {
+        location.reload();
+        })
 
     $(".charImg1").on("click", function() {                                                  // click rick                          
         rick1();                                                                            // call the rick1 function to move rick into position 1
@@ -121,25 +153,30 @@ $(document).ready(function(){
         move1 = 4123;                                                                       // set var move1 to 4123
     });
 
-/*
+    $(".attack").on("click", function() {
 
-    $(".r3c1").on("click", function() {                                                     // If you click on position 1 (Enemies Available position)
-        movePos1D();                                                                        // Move this character to the Defender position
+        yourDefend.HP = yourDefend.HP - yourChar.Attack       //make function
+        yourChar.HP = yourChar.HP - yourDefend.CounterAttack
+        if (yourChar.HP < 1) {
+            alert("You Suck - you have lost the game");
+            
+            } else if (yourDefend.HP < 1) {
+                $("P").html("Enemy Defeated");
+            } else {
+        $("P").html("You Attacked for " + yourChar.Attack + " damage, you were Counter Attacked for " + yourDefend.CounterAttack);
+        $("#HP1").html("HP: " + (rick.HP));                                        
+        $("#HP2").html("HP: " + (morty.HP));                            
+        $("#HP3").html("HP: " + (krom.HP));                
+        $("#HP4").html("HP: " + (lucius.HP));
+         
+        }
     });
 
-    $(".r3c2").on("click", function() {                                                     // If you click on position 2 (Enemies Available postion)
-        movePos2D();                                                                        // Move this character to the Defender position
-    });
+        //alert("you clicked the button");
+       //location.reload();
+    
 
-    $(".r3c3").on("click", function() {                                                     // If you click on position 3 (Enemies Available position)
-        movePos3D();                                                                        // Move this character to the Defender position
-    });
-
-
-*/
-
-
-
+ 
 
 
 
